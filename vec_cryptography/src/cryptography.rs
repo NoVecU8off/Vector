edcd60649e58510d04482de9055030b85f7c8331
Andrew Novikoff <AndrewNovikoff@outlook.com>
2023-05-29 14:38:51 +0400

    Developing keys from scratch

--- vec_cryptography/src/cryptography.rs
+++ vec_cryptography/src/cryptography.rs
@@ -1,194 +1,204 @@
-use ed25519_dalek::{SecretKey, ExpandedSecretKey, PublicKey, Signature as Ed25519Signature};
-use ed25519_dalek::{Verifier};
-use rand::{Rng, thread_rng};
-use rand::{rngs::OsRng, RngCore};
-use sha3::{Sha3_256, Digest};
-use arrayref::{array_ref};
-use std::fmt;
-use serde::{Serializer, Deserializer, Serialize, Deserialize, de::Error as DeError};
+use curve25519_dalek_ng::scalar::Scalar;
+use curve25519_dalek_ng::edwards::CompressedEdwardsY;
+use curve25519_dalek_ng::constants;
+use sha3::{Keccak256, Digest};
+use bs58;
 
-pub fn generate_seed_thread() -> [u8; 32] {
-    let mut threaded_seed = [0u8; 32];
-    let mut rng = thread_rng();
-    rng.fill(&mut threaded_seed);
-    threaded_seed
+struct Wallet {
+    secret_spend_key: Scalar,
+    secret_view_key: Scalar,
+    public_spend_key: CompressedEdwardsY,
+    public_view_key: CompressedEdwardsY,
+    address: String,
 }
 
-pub fn generate_seed_os() -> [u8; 32] {
-    let mut os_seed = [0u8; 32];
-    let mut rng = OsRng;
-    rng.fill_bytes(&mut os_seed);
-    os_seed
-}
-
-pub fn inherit_seed() -> [u8; 32] {
-    let t_seed = generate_seed_thread();
-    let o_seed = generate_seed_os();
-    let mut hasher = Sha3_256::new();
-    hasher.update(array_ref![t_seed, 0, 32]);
-    hasher.update(array_ref![o_seed, 0, 32]);
-    let hash = hasher.finalize();
-    let mut seed = [0u8; 32];
-    seed.copy_from_slice(&hash[..32]);
-    seed
-}
-pub struct NodeKeypair {
-    pub sk: SecretKey,
-    pub expanded_sk_key: ExpandedSecretKey,
-    pub pk: PublicKey,
-}
-
-impl NodeKeypair {
-    pub fn generate_keypair() -> Self {
-        let seed = inherit_seed();
-        let sk_key = SecretKey::from_bytes(&seed).unwrap();
-        let expanded_secret_key = ExpandedSecretKey::from(&sk_key);
-        let pk = PublicKey::from(&expanded_secret_key);
-        NodeKeypair {
-            sk: sk_key,
-            expanded_sk_key: expanded_secret_key,
-            pk: pk,
-        }
-    }
-
-    pub fn sign(&self, message: &[u8]) -> Signature {
-        let signature = self.expanded_sk_key.sign(message, &self.pk);
-        Signature {
-            signature: signature,
-        }
-    }
-
-    pub fn verify(&self, message: &[u8], signature: &Signature) -> bool { 
-        self.pk.verify(message, &signature.signature).is_ok()
-    }
-
-    pub fn public_to_vec(&self) -> Vec<u8> {
-        let vec_public = self.pk.as_bytes().to_vec();
-        vec_public
-    }
-
-    pub fn pk_from_vec(vec_public: &[u8]) -> PublicKey {
-        PublicKey::from_bytes(vec_public).unwrap()
-    }
-}
-
-impl Clone for NodeKeypair {
-    fn clone(&self) -> Self {
-        let sk = SecretKey::from_bytes(&self.sk.to_bytes()).expect("Unable to clone SecretKey");
-        let expanded_sk_key = ExpandedSecretKey::from_bytes(&self.expanded_sk_key.to_bytes()).expect("Unable to clone ExpandedSecretKey");
-        let pk = PublicKey::from_bytes(&self.pk.to_bytes()).expect("Unable to clone PublicKey");
-        Self {
-            sk,
-            expanded_sk_key,
-            pk,
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Clone, Copy)]
 pub struct Signature {
-    pub signature: Ed25519Signature,
+    r: CompressedEdwardsY,
+    s: Scalar,
 }
 
-impl Signature {
-    pub fn to_vec(&self) -> Vec<u8> {
-        self.signature.as_ref().to_vec()
-    }
+impl Wallet {
+    fn generate() -> Wallet {
+        let mut rng = rand::thread_rng();
 
-    pub fn to_bytes(&self) -> [u8; 64] {
-        let mut bytes = [0u8; 64];
-        bytes.copy_from_slice(self.signature.as_ref());
-        bytes    
-    }
+        // Generate a random secret spend key
+        let secret_spend_key: Scalar = Scalar::random(&mut rng);
 
-    pub fn from_bytes(bytes: [u8; 64]) -> Self {
-        Self {
-            signature: Ed25519Signature::from_bytes(&bytes).unwrap()
+        // Derive the secret view key by hashing the secret spend key and reducing modulo l
+        let mut hasher = Keccak256::new();
+        hasher.update(secret_spend_key.as_bytes());
+        let hashed_key = hasher.finalize();
+        let secret_view_key = Scalar::from_bytes_mod_order(hashed_key.into());
+
+        // Calculate the public keys by multiplying the secret keys by the basepoint of the elliptic curve
+        let public_spend_key = &constants::ED25519_BASEPOINT_TABLE * &secret_spend_key;
+        let public_view_key = &constants::ED25519_BASEPOINT_TABLE * &secret_view_key;
+
+        // Prepare the address data (public spend key + public view key)
+        let mut data = vec![];
+        data.extend(public_spend_key.compress().as_bytes());
+        data.extend(public_view_key.compress().as_bytes());
+
+        // Add the checksum (the first four bytes of the Keccak-256 hash of the address data)
+        let mut hasher = Keccak256::new();
+        hasher.update(&data);
+        let hash = hasher.finalize();
+        data.extend(&hash[0..4]);
+
+        // Encode the address data as a Base58 string
+        let address = bs58::encode(&data).into_string();
+
+        Wallet {
+            secret_spend_key,
+            secret_view_key,
+            public_spend_key: public_spend_key.compress(),
+            public_view_key: public_view_key.compress(),
+            address,
         }
     }
 
-    pub fn v_to_bytes(&self) -> [u8; 64] {
-        vec_to_bytes(&self.signature.as_ref().to_vec())
-    }
+    fn reconstruct(secret_spend_key: Scalar) -> Wallet {
+        // Derive the secret view key by hashing the secret spend key and reducing modulo l
+        let mut hasher = Keccak256::new();
+        hasher.update(secret_spend_key.as_bytes());
+        let hashed_key = hasher.finalize();
+        let secret_view_key = Scalar::from_bytes_mod_order(hashed_key.into());
 
-    pub fn signature_from_vec(vec_signature: &[u8]) -> Signature {
-        let mut bytes = [0u8; 64];
-        bytes.copy_from_slice(vec_signature);
-        Signature {
-            signature: Ed25519Signature::from_bytes(&bytes).unwrap(),
+        // Calculate the public keys by multiplying the secret keys by the basepoint of the elliptic curve
+        let public_spend_key = &constants::ED25519_BASEPOINT_TABLE * &secret_spend_key;
+        let public_view_key = &constants::ED25519_BASEPOINT_TABLE * &secret_view_key;
+
+        // Prepare the address data (public spend key + public view key)
+        let mut data = vec![];
+        data.extend(public_spend_key.compress().as_bytes());
+        data.extend(public_view_key.compress().as_bytes());
+
+        // Add the checksum (the first four bytes of the Keccak-256 hash of the address data)
+        let mut hasher = Keccak256::new();
+        hasher.update(&data);
+        let hash = hasher.finalize();
+        data.extend(&hash[0..4]);
+
+        // Encode the address data as a Base58 string
+        let address = bs58::encode(&data).into_string();
+
+        Wallet {
+            secret_spend_key,
+            secret_view_key,
+            public_spend_key: public_spend_key.compress(),
+            public_view_key: public_view_key.compress(),
+            address,
         }
     }
-}
 
-impl std::fmt::Display for Signature {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", hex::encode(self.signature.to_bytes()))
+    fn sign(&self, message: &[u8]) -> Signature {
+        let mut rng = rand::thread_rng();
+    
+        // Step 1: Generate a random nonce
+        let nonce = Scalar::random(&mut rng);
+    
+        // Step 2: Calculate the point R = nonce * basepoint
+        let r_ep = &constants::ED25519_BASEPOINT_TABLE * &nonce;
+        let r = r_ep.compress();
+    
+        // Step 3: Hash the concatenation of R, the public key, and the message
+        let mut hasher = Keccak256::new();
+        hasher.update(r_ep.compress().as_bytes());
+        hasher.update(self.public_spend_key.as_bytes());
+        hasher.update(message);
+        let h = hasher.finalize();
+    
+        // Step 4: Convert the hash to a scalar
+        let h_scalar = Scalar::from_bits(h.into());
+    
+        // Step 5: Calculate s = nonce - hash * secret_key
+        let s = nonce - h_scalar * self.secret_spend_key;
+    
+        Signature { r, s }
+    }
+    
+    fn verify(public_spend_key: CompressedEdwardsY, message: &[u8], signature: Signature) -> bool {
+        // Step 1: Decompress the R value and public key
+        let r = signature.r.decompress().unwrap();
+        let public_spend_key_point = public_spend_key.decompress().unwrap();
+    
+        // Step 2: Hash the concatenation of R, the public key, and the message
+        let mut hasher = Keccak256::new();
+        hasher.update(signature.r.as_bytes());
+        hasher.update(public_spend_key.as_bytes());
+        hasher.update(message);
+        let h = hasher.finalize();
+    
+        // Step 3: Convert the hash to a scalar
+        let h_scalar = Scalar::from_bits(h.into());
+    
+        // Step 4: Calculate the expected R value: R' = s * basepoint + h * public_key
+        let r_prime = &constants::ED25519_BASEPOINT_TABLE * &signature.s + public_spend_key_point * &h_scalar;
+    
+        // Step 5: Verify the signature by checking if R equals R'
+        r == r_prime
     }
 }
 
-impl std::convert::From<[u8; 64]> for Signature {
-    fn from(bytes: [u8; 64]) -> Self {
-        Self::from_bytes(bytes)
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_wallet_generation() {
+        let wallet = Wallet::generate();
+
+        // Check if the secret and public keys were generated properly
+        assert_ne!(wallet.secret_spend_key, Scalar::zero());
+        assert_ne!(wallet.secret_view_key, Scalar::zero());
+        assert_ne!(*wallet.public_spend_key.as_bytes(), [0; 32]);
+        assert_ne!(*wallet.public_view_key.as_bytes(), [0; 32]);
+
+        // Check if the public keys are correct multiples of the secret keys
+        assert_eq!(wallet.public_spend_key, (&constants::ED25519_BASEPOINT_TABLE * &wallet.secret_spend_key).compress());
+        assert_eq!(wallet.public_view_key, (&constants::ED25519_BASEPOINT_TABLE * &wallet.secret_view_key).compress());
+
+        // Check if the address is properly encoded
+        let decoded_address = bs58::decode(&wallet.address).into_vec().unwrap();
+        assert_eq!(decoded_address[0..32], wallet.public_spend_key.as_bytes()[..]);
+        assert_eq!(decoded_address[32..64], wallet.public_view_key.as_bytes()[..]);
     }
-}
 
-pub fn vec_to_bytes(vec: &Vec<u8>) -> [u8; 64] {
-    let mut bytes = [0u8; 64];
-    let num_bytes = std::cmp::min(vec.len(), 64);
-    bytes[..num_bytes].copy_from_slice(&vec[..num_bytes]);
-    bytes
-}
+    #[test]
+    fn test_wallet_signature() {
+        let wallet = Wallet::generate();
+        let message = b"Hello, World!";
 
-impl std::fmt::Display for NodeKeypair {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?}, {:?}", self.sk, self.pk)
+        // Sign the message
+        let signature = wallet.sign(message);
+
+        // Check if the signature is valid
+        assert!(Wallet::verify(wallet.public_spend_key, message, signature));
+
+        // Check if the signature is invalid for a different message
+        let different_message = b"Goodbye, World!";
+        assert!(!Wallet::verify(wallet.public_spend_key, different_message, signature));
+
+        // Check if the signature is invalid for a different key
+        let different_wallet = Wallet::generate();
+        assert!(!Wallet::verify(different_wallet.public_spend_key, message, signature));
     }
-}
 
-impl fmt::Debug for NodeKeypair {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Keypair")
-            .field("sk", &self.sk)
-            .field("pk", &self.pk)
-            .finish()
-    }
-}
+    #[test]
+    fn test_wallet_reconstruction() {
+        // Generate a new wallet
+        let wallet = Wallet::generate();
+        
+        // Reconstruct the wallet using only the secret spend key
+        let reconstructed_wallet = Wallet::reconstruct(wallet.secret_spend_key);
 
-#[derive(Serialize, Deserialize)]
-struct SerializableKeypair {
-    sk: Vec<u8>,
-    expanded_sk_key: Vec<u8>,
-    pk: Vec<u8>,
-}
-
-impl Serialize for NodeKeypair {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        let serializable = SerializableKeypair {
-            sk: self.sk.to_bytes().to_vec(),
-            expanded_sk_key: self.expanded_sk_key.to_bytes().to_vec(),
-            pk: self.pk.to_bytes().to_vec(),
-        };
-        serializable.serialize(serializer)
-    }
-}
-
-impl<'de> Deserialize<'de> for NodeKeypair {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        let serializable = SerializableKeypair::deserialize(deserializer)?;
-        let sk = SecretKey::from_bytes(&serializable.sk).map_err(DeError::custom)?;
-        let expanded_sk_key = ExpandedSecretKey::from_bytes(&serializable.expanded_sk_key).map_err(DeError::custom)?;
-        let pk = PublicKey::from_bytes(&serializable.pk).map_err(DeError::custom)?;
-        let keypair = NodeKeypair {
-            sk,
-            expanded_sk_key,
-            pk,
-        };
-        Ok(keypair)
+        // Check if the reconstructed wallet matches the original
+        assert_eq!(wallet.secret_spend_key, reconstructed_wallet.secret_spend_key);
+        assert_eq!(wallet.secret_view_key, reconstructed_wallet.secret_view_key);
+        assert_eq!(*wallet.public_spend_key.as_bytes(), *reconstructed_wallet.public_spend_key.as_bytes());
+        assert_eq!(*wallet.public_view_key.as_bytes(), *reconstructed_wallet.public_view_key.as_bytes());
+        assert_eq!(wallet.address, reconstructed_wallet.address);
     }
 }
